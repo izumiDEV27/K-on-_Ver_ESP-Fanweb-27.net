08acfb11bd46da00e1727eca5a9e9a876dfc4a5e
Hizui23 <gustavolexytofficial@gmail.com>
2026-01-21 18:25:17 -0600

    update2026/21enero

--- script_lobby.js
+++ script_lobby.js
@@ -14,3 +14,18 @@ function ver_manga(url) {
         alert("¡Aún no hay link de descarga!");
     }
 }
+
+// parte de mensaje emergente del codigo
+const popupOverlay = document.getElementById('popupOverlay');
+        const closeBtn = document.getElementById('closePopup');
+
+        closeBtn.addEventListener('click', () => {
+            popupOverlay.style.display = 'none'; // Oculta el pop-up
+        });
+
+        // Opcional: cerrar también al hacer click fuera de la caja
+        popupOverlay.addEventListener('click', (e) => {
+            if(e.target === popupOverlay) {
+                popupOverlay.style.display = 'none';
+            }
+        });
