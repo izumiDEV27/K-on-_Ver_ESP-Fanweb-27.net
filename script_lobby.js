83a7a31d3b273acda26c72044b465e0acecb34a3
Hizui23 <gustavolexytofficial@gmail.com>
2026-02-13 13:56:05 -0600

    add colaboradores

--- script_lobby.js
+++ script_lobby.js
@@ -60,3 +60,18 @@ document.addEventListener('DOMContentLoaded', () => {
     setInterval(moverAleatoriamente, 2000);
 });
 //que ves chismoso, hahahah, esta web es estatica, no me pidad milagros
+
+//implementacion de scroll
+function moverCarrusel(direccion) {
+    const track = document.getElementById('track');
+    // Obtenemos el ancho de un elemento para saber cuánto desplazar
+    const anchoItem = track.querySelector('.carrusel-item').clientWidth;
+    // Sumamos el gap (20px en el CSS) para que sea exacto
+    const desplazamiento = anchoItem + 20; 
+
+    // Mueve el scroll: direccion 1 es derecha, -1 es izquierda
+    track.scrollBy({
+        left: direccion * desplazamiento, 
+        behavior: 'smooth' 
+    });
+}
